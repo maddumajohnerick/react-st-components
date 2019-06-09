4bb42a554762feba802a6045c19cddf795154e88
Erick Madduma <e.madduma@arcanys.com>
2019-06-09 17:45:43 +0800

    added publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
+    publicPath: path.resolve(__dirname, 'docs')
   },
   module: {
     rules: [
--- webpack.docsConfig.js
+++ webpack.docsConfig.js
@@ -12,6 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'docs.js',
+    publicPath: path.resolve(__dirname, 'docs')
   },
   module: {
     rules: [
